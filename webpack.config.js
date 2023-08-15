c9c2f27c75ed9334db7c83ce97c0837a35ff3afb
f.sorokin <f.sorokin@fsorokin-nb.local>
2023-08-15 19:03:12 +0300

    initial

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+module.exports = {
+  name: "dummy_project",
+  mode: "development",
+  entry: path.join(__dirname, "generated", "index.js"),
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?x?$/,
+        use: [
+          // {
+          //   loader: "babel-loader",
+          //   options: {
+          //     presets: [["@babel/preset-env", { targets: "defaults" }], "@babel/preset-react"],
+          //   },
+          // },
+          {
+            loader: "swc-loader",
+            options: {
+              jsc: {
+                parser: { syntax: "ecmascript", jsx: true },
+              },
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
