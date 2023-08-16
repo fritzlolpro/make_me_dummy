9350784843cfab1f7d748833a4889b64c330ac8f
f.sorokin <f.sorokin@fsorokin-nb.local>
2023-08-16 17:21:19 +0300

    classy

--- src/generate.mjs
+++ src/generate.mjs
@@ -1,10 +1,103 @@
 import fs from "node:fs/promises";
 import path from "node:path";
 
+class OutputAdapter {
+  constructor(outputter, DataProvider) {
+    this.outputter = outputter;
+    this.data = DataProvider.getData();
+  }
+
+  async apply() {
+    await this.outputter.apply(null, this.data);
+  }
+}
+
+class FileWriter {
+  constructor(path, data) {
+    this.path = path;
+    this.data = data;
+    this.writer = new OutputAdapter(fs.writeFile, this);
+  }
+  getData() {
+    return [this.path, this.data];
+  }
+  async apply() {
+    await this.writer.apply();
+  }
+}
+
+class ReactJsComponentStringBuilder {
+  constructor(postfix, fileName) {
+    this.postfix = postfix;
+    this.fileName = fileName;
+  }
+
+  getTemplate() {
+    return `
+      import React from 'react'
+      export const Component_${this.postfix} = () => <div> This is a Component </div>;
+    `;
+  }
+
+  getUseString() {
+    return `<Component_${this.postfix}/> \n`;
+  }
+
+  getImportString() {
+    return `import {Component_${this.postfix}} from './${this.getFileName()}' \n`;
+  }
+
+  getFileName() {
+    return `${this.fileName}_${this.postfix}.jsx`;
+  }
+}
+
+class TempalateFactory {
+  constructor(name, quantity, Builder) {
+    this.quantity = quantity;
+    this.name = name;
+    this.builder = Builder;
+    this.importTemplate = "";
+    this.useTemplate = "";
+    this.fileTemplates = [];
+    this.fileNames = [];
+    this.generate();
+  }
+
+  generate() {
+    let i = 0;
+    while (i < this.quantity) {
+      const stringInstance = new this.builder(i, this.name);
+      this.fileNames.push(stringInstance.getFileName());
+      this.fileTemplates.push(stringInstance.getTemplate());
+      this.useTemplate += stringInstance.getUseString();
+      this.importTemplate += stringInstance.getImportString();
+      i++;
+    }
+  }
+
+  getImportsTemplate() {
+    return this.importTemplate;
+  }
+  getUseTemplate() {
+    return this.useTemplate;
+  }
+  getFileTemplates() {
+    return this.fileTemplates;
+  }
+  getFileNames() {
+    return this.fileNames;
+  }
+  getGeneratedQuantity() {
+    return this.quantity;
+  }
+}
+
 (async () => {
   console.log(process.cwd());
 
-  const folder = path.resolve(process.cwd(), "generated");
+  const folder = path.resolve(process.cwd(), "../generated");
+  const rootFolder = path.resolve(process.cwd(), "../");
   const REACT_JS_COMPONENT_QUANTITY = 5;
 
   try {
@@ -13,20 +106,21 @@ import path from "node:path";
     await fs.mkdir(folder);
   }
 
-  let i = 0;
-  let reactJsComponentsImportString = "";
-  let reactJsComponentsUseString = "";
-  while (i < REACT_JS_COMPONENT_QUANTITY) {
-    const fileName = `component_${i}.jsx`;
-    const template = `
-      import React from 'react'
-      export const Component_${i} = () => <div> THis ias Component </div>;
-  `;
+  const reactComponents = new TempalateFactory(
+    "component",
+    10,
+    ReactJsComponentStringBuilder
+  );
 
-    reactJsComponentsImportString += `import {Component_${i}} from './${fileName}' \n`;
-    reactJsComponentsUseString += `<Component_${i} /> \n`;
-    await fs.writeFile(path.resolve(folder, fileName), template);
-    i++;
+  for (let i = 0; i < reactComponents.getGeneratedQuantity(); i++) {
+    (async () => {
+      console.log(reactComponents.getFileNames())
+      const writer = new FileWriter(
+        path.resolve(folder, reactComponents.getFileNames()[i]),
+        reactComponents.getFileTemplates()[i]
+      );
+      await writer.apply();
+    })();
   }
 
   const indexJS = "index.js";
@@ -35,14 +129,14 @@ import path from "node:path";
     import React from 'react'
     import { createRoot } from 'react-dom/client';
 
-    ${reactJsComponentsImportString}
+    ${reactComponents.getImportsTemplate()}
 
     const test = () => <div className="className penis"> AAAAAAA </div>
     test();
     const root = createRoot(document.getElementById('app'));
     root.render(
       <div>
-        ${reactJsComponentsUseString}
+        ${reactComponents.getUseTemplate()}
       </div>
     );
 
@@ -64,5 +158,5 @@ import path from "node:path";
 `;
 
   await fs.writeFile(path.resolve(folder, indexJS), indexTemplate);
-  await fs.writeFile(path.resolve(process.cwd(), indexHtml), htmlRoot);
+  await fs.writeFile(path.resolve(rootFolder, indexHtml), htmlRoot);
 })();
